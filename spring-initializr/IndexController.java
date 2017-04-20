1080d92a52cafb3b0decfe8edca7759e56972bc7
Per Hammer <github@perhammer.com>
2017-04-20 08:34:46 +0100

    Stripped model

--- spring-initializr/IndexController.java
+++ spring-initializr/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 public class IndexController {
-    String index(Model model){
+    String index(){
         return "index";
     }
 }
