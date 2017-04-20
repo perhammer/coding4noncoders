d8930f6a4b809e8296b1eb86dc062733072409a0
Per Hammer <github@perhammer.com>
2017-04-20 08:11:06 +0100

    Removed the fun bits@

--- spring-initializr/IndexController.java
+++ spring-initializr/IndexController.java
@@ -4,11 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
-@Controller
 public class IndexController {
-    @RequestMapping("/")
     String index(Model model){
-        model.addAttribute("hello", "dolly");
         return "index";
     }
 }
